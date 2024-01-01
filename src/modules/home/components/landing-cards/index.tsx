cc72572589177cc78a0eb5344f94bd6aa467e3fc
Joshua Lehman <joshlehman.dev@gmail.com>
2024-01-01 09:39:51 -0500

    updates

---
+++ src/modules/home/components/landing-cards/index.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import React from "react"
+import clsx from "clsx"
+import { CardType } from "../../../../types/types"
+// import { CldImage } from "next-cloudinary"
+import styles from "./landing-cards.module.css"
+import RoundButton from "@modules/common/components/round-button-link"
+
+const LandingCard = ({ card }: CardType) => {
+  const imageTernary =
+    card.className === "landing_card_one"
+      ? "BeanbagCoffee/expresso_jn47nr"
+      : card.className === "landing_card_two"
+      ? "BeanbagCoffee/coldbrew_mt3tia"
+      : card.className === "landing_card_three"
+      ? "BeanbagCoffee/bagbean_pm95hy"
+      : ""
+
+  return (
+    <div className={styles.flex_container}>
+      <div className={styles.spacer} />
+      <div className={styles.left_column}>
+        <div
+          className={clsx({
+            [styles.landing_card_one]: card.className === "landing_card_one",
+            [styles.landing_card_two]: card.className === "landing_card_two",
+            [styles.landing_card_three]:
+              card.className === "landing_card_three",
+          })}
+        >
+          <div className={styles.spacer_large} />
+          <h2 className={styles.card_title}>{card.title}</h2>
+          <div className={styles.spacer} />
+          <h4 className={styles.card_text_body}>{card.textBody}</h4>
+          <div className={styles.spacer} />
+          <RoundButton
+            href=""
+            className={clsx({
+              [styles.button_card_one]: card.className === "landing_card_one",
+              [styles.button_card_two]: card.className === "landing_card_two",
+              [styles.button_card_three]:
+                card.className === "landing_card_three",
+            })}
+          >
+            {card.buttonText}
+          </RoundButton>
+        </div>
+      </div>
+      <div className={styles.right_column}>
+        <div>
+          {/* <CldImage
+            crop="thumb"
+            alt={imageTernary}
+            width="600"
+            height="600"
+            src={imageTernary}
+          /> */}
+        </div>
+      </div>
+      <div className={styles.spacer} />
+    </div>
+  )
+}
+
+export default LandingCard
---
+++ src/modules/skeletons/components/skeleton-homepage-products copy/index.tsx
@@ -0,0 +1,8 @@
+import SkeletonProductPreview from "../skeleton-product-preview"
+
+
+const SkeletonHomepageProducts = () => {
+  return <SkeletonProductPreview />
+}
+
+export default SkeletonHomepageProducts
