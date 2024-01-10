0d64945a971a210ebf297d01a35ceedd042271e6
Joshua Lehman <joshlehman.dev@gmail.com>
2024-01-09 21:05:05 -0500

    updates

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -19,7 +19,27 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
-    images: "/og-image.png",
+    title: "Beanbag Coffee Co. Online Storefront",
+    description:
+      "A performant ecommerce store built with Next.js 14 and Medusa.",
+    url: "https://storefront-git-main-joshl26.vercel.app/",
+    siteName: "Beanbag Coffee Co.",
+    images: [
+      {
+        url: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_PUBLIC_CLOUD_ID}/image/upload/v1704851738/BeanbagCoffee/beanbag-coffee-1_w4gcxv.png`,
+        width: 1885,
+        height: 965,
+        alt: "Beanbag Coffee Co. Drink Menu",
+      },
+      {
+        url: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_PUBLIC_CLOUD_ID}/image/upload/v1704851736/BeanbagCoffee/beanbag-coffee-3_sk8qso.png`,
+        width: 1887,
+        height: 965,
+        alt: "Beanbag Coffee Co. Cart",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
   title: "Beanbag Coffee Co. Online Storefront",
   description: "A performant ecommerce store built with Next.js 14 and Medusa.",
