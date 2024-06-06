82768b3ded780e212516abc33bc7a4dc35d25856
Joshua Lehman <joshlehman.dev@gmail.com>
2024-06-06 08:22:45 -0400

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-mode="light">
+      <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-2880932731702994"
+        ></meta>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2880932731702994"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body>
         <Providers>
           <main className="relative">{children}</main>
